8272f4ab6a326ffe384f715fc0be73f94c8b548e
TheELLCRO <TheELLECRO@ya.ru>
2017-05-17 21:01:19 +0300

    Upload

---
+++ Meridian/Extensions/AudioExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LastFmLib.Core.Track;
+using Meridian.Model;
+using Meridian.ViewModel;
+
+namespace Meridian.Extensions
+{
+    public static class AudioExtensions
+    {
+        public static VkAudio ToAudio(this VkLib.Core.Audio.VkAudio audio)
+        {
+            var result = new VkAudio();
+            result.Id = audio.Id.ToString();
+            result.Title = audio.Title;
+            result.Artist = audio.Artist;
+            result.AlbumId = audio.AlbumId;
+            result.Duration = audio.Duration;
+            result.LyricsId = audio.LyricsId;
+            result.OwnerId = audio.OwnerId;
+            result.Source = audio.Url;
+            result.GenreId = audio.GenreId;
+            result.IsAddedByCurrentUser = audio.OwnerId == ViewModelLocator.Vkontakte.AccessToken.UserId;
+
+            return result;
+        }
+
+        public static VkAudio ToAudio(this LastFmTrack audio)
+        {
+            var result = new VkAudio();
+            result.Title = audio.Title;
+            result.Artist = audio.Artist;
+            result.Duration = TimeSpan.FromSeconds(audio.Duration);
+
+            return result;
+        }
+    }
+}
